d20058891bef5b18f89c8aec6561390b4d486315
Fabien Matusalem <matusalemf@aol.com>
2024-01-28 15:12:52 +0100

    Added hiding of quit button in main menu

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -5,22 +5,25 @@ extends Control
 @export var level2: PackedScene
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var _button_quit: Button = %ButtonQuit
+
+
 func _ready() -> void:
 	$VBoxContainer/ButtonLevel1.grab_focus()
+	_button_quit.visible = OS.get_name() != "Web"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
 
 func _on_button_level1_pressed() -> void:
 	get_tree().change_scene_to_packed(level1)
 
+
 func _on_button_level2_pressed() -> void:
 	get_tree().change_scene_to_packed(level2)
 
+
 func _on_button_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/credits_menu.tscn")
+
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
