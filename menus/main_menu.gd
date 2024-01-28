49e0e9fb06ee34ec81cf016b207d8a5e7b7827f4
Th0re <stephane.perrez@gmail.com>
2024-01-28 14:08:37 +0100

    Added credits and level2 in main menu

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,22 +1,26 @@
 extends Control
 
 
-@export var main_level: PackedScene
+@export var level1: PackedScene
+@export var level2: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/ButtonPlay.grab_focus()
+	$VBoxContainer/ButtonLevel1.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_packed(main_level)
+func _on_button_level1_pressed() -> void:
+	get_tree().change_scene_to_packed(level1)
+
+func _on_button_level2_pressed() -> void:
+	get_tree().change_scene_to_packed(level2)
 
 func _on_button_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://menus/settings_menu.tscn")
+	get_tree().change_scene_to_file("res://menus/credits_menu.tscn")
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
